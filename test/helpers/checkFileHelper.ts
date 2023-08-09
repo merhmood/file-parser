e07ce7219e571024cbb6c834db1eac0d7ac1c907
merhmood <mahmudsuberu9@gmail.com>
2023-08-09 08:12:09 +0100

    build: remove problems blocking build
    remove es5 key from env in .eslintrc.json
    remove fileValidator module and replace with jest.fn

--- test/helpers/checkFileHelper.ts
+++ test/helpers/checkFileHelper.ts
@@ -1,11 +1,10 @@
 import FakeFileClass from '../mocks/file';
-import fileValidator from '../mocks/fileValidator';
 
 function checkFileHelper() {
   let stubFile = FakeFileClass;
   let result = {
     file: stubFile,
-    fileValidator: fileValidator,
+    fileValidator: jest.fn(),
     setWorkbook: jest.fn(),
     setTableContent: jest.fn(),
     setLoadmoreCursor: jest.fn(),
