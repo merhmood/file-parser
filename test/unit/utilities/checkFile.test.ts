e9fa103204b5f95451d4b4db7d9a201d8ba6d395
merhmood <mahmudsuberu9@gmail.com>
2023-08-09 15:52:33 +0100

    test: add more test to checkFile.test.ts and set npm run test to watch mode

--- test/unit/utilities/checkFile.test.ts
+++ test/unit/utilities/checkFile.test.ts
@@ -1,9 +1,30 @@
 import checkFile from '@/utilities/checkFile';
 import checkFileHelper from '../../helpers/checkFileHelper';
 
-test('writing test to see if filevalidator is called with file', () => {
+test('check filevalidator is called with file', () => {
   const fakeCheckFileParameters: any = checkFileHelper();
   const mockFileValidator = fakeCheckFileParameters.fileValidator;
   checkFile(fakeCheckFileParameters);
   expect(mockFileValidator).toHaveBeenCalledWith(fakeCheckFileParameters.file);
+});
+
+test('check if isvalid block calls functions with correct parameters', () => {
+  const fakeCheckFileParameters: any = checkFileHelper();
+  const mockFileHandler = fakeCheckFileParameters.fileHandler;
+  const mockSetInvalidFile = fakeCheckFileParameters.setInvalidFile;
+  const stubfileValidator = () => true;
+  checkFile({
+    ...fakeCheckFileParameters,
+    fileHandler: mockFileHandler,
+    setInvalidFile: mockSetInvalidFile,
+    fileValidator: stubfileValidator,
+  });
+
+  expect(mockFileHandler).toBeCalledWith(
+    fakeCheckFileParameters.file,
+    fakeCheckFileParameters.setWorkbook,
+    fakeCheckFileParameters.setTableContent,
+    fakeCheckFileParameters.setLoadmoreCursor
+  );
+  expect(mockSetInvalidFile).toBeCalledWith(false);
 });
